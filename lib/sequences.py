3ec56f8f200b535640b783ce2435c43620f368bd
Elchonon <ekn400@gmail.com>
2024-12-10 10:18:35 -0500

    lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
+def get_fibonacci(number):
+    return 
+
+
+'''
+start list with 0,1
+for range of length/2-2 
+get last 2 add up and append to list
+
+'''
 def print_fibonacci(length):
-    pass
+    if length == 0: 
+        print([])
+    elif length == 1:
+        print([0])
+    else: 
+        fib_list = [0,1]
+        for i in range(int(length-2)):
+            fib_list.append(fib_list[-1] + fib_list[-2])
+        print(fib_list)
+
+print_fibonacci(8)
